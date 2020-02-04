629772040b243ff0495294a8bb84f1aa7d67e211
narago10 <nanararagogo10@gmail.com>
2020-02-04 13:26:24 +0900

    비밀 정보 보안

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const app = express()
-const port = 3000
+const port = 4000
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://DevDobin:dobin2000@devdobin-90xe1.mongodb.net/test?retryWrites=true&w=majority', {
-    // https://www.youtube.com/watch?v=IHjolKwrjPE 4:58
-    // https://www.youtube.com/watch?v=wMJ7hQ2FKfQ
+const config = require('./config/key')
+
+// bodyParser 사용하기 위해서
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//
+app.use(bodyParser.urlencoded({extended: true}));
+//
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose') 
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!!'))
+
+
+app.post('/register', (req, res) => {
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+    // https://www.youtube.com/watch?v=IHjolKwrjPE 4:58
+    // https://www.youtube.com/watch?v=wMJ7hQ2FKfQ
--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema({ 
     name: {
         type: String,
         maxlength: 50
@@ -29,4 +29,4 @@ const userSchema = mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-module.exports = { User };
+module.exports = { User }
